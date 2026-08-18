9062d77f7241813c7bc3e44135638c09621910f0
Aman Varshney <amanvarshney.work@gmail.com>
2026-08-18 19:52:07 +0530

    chore: update Composer packages to 0.9

--- tests/dependencies.test.ts
+++ tests/dependencies.test.ts
@@ -3,11 +3,11 @@ import { describe, expect, test } from "bun:test";
 import { dependencyVersionMap, getDependencyVersion } from "../src/constants/dependencies";
 
 describe("Prisma 8 dependency versions", () => {
-  test("uses the aligned Prisma 8 and Composer releases", () => {
+  test("uses the selected Prisma 8 and Composer releases", () => {
     expect(getDependencyVersion("@prisma/orm-postgres")).toBe("8.0.0-rc.3");
     expect(getDependencyVersion("@prisma/cli-engine")).toBe("0.2.0");
-    expect(getDependencyVersion("@prisma/composer")).toBe("0.8.0");
-    expect(getDependencyVersion("@prisma/composer-prisma-cloud")).toBe("0.8.0");
+    expect(getDependencyVersion("@prisma/composer")).toBe("0.9.0");
+    expect(getDependencyVersion("@prisma/composer-prisma-cloud")).toBe("0.9.0");
   });
 
   test("returns undefined for dependencies missing from the version map", () => {
