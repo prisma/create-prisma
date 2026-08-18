5f13a08abebd768070f5a00597883779d139c5f3
Aman Varshney <amanvarshney.work@gmail.com>
2026-08-18 17:24:53 +0530

    fix: consume released framework deployment fixes
    Signed-off-by: Aman Varshney <amanvarshney.work@gmail.com>

--- tests/dependencies.test.ts
+++ tests/dependencies.test.ts
@@ -8,8 +8,8 @@ describe("Prisma 8 dependency versions", () => {
     expect(getDependencyVersion("@prisma/orm-postgres")).toBe("8.0.0-rc.1");
     expect(getDependencyVersion("@prisma/cli")).toBe("8.0.0-rc.2");
     expect(getDependencyVersion("@prisma/cli-engine")).toBe("8.0.0-rc.2");
-    expect(getDependencyVersion("@prisma/composer")).toBe("0.6.0-dev.18");
-    expect(getDependencyVersion("@prisma/composer-prisma-cloud")).toBe("0.6.0-dev.18");
+    expect(getDependencyVersion("@prisma/composer")).toBe("0.7.0");
+    expect(getDependencyVersion("@prisma/composer-prisma-cloud")).toBe("0.7.0");
   });
 
   test("returns undefined for dependencies missing from the version map", () => {
